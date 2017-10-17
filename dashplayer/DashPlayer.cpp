646856231a4c9fb374d49e1b87e80e18eff227a3
sujit roy <kumarsujitroy@gmail.com>
2017-10-17 06:58:52 +0000

    dashplayer: fix utf16_to_utf8 for asb

--- dashplayer/DashPlayer.cpp
+++ dashplayer/DashPlayer.cpp
@@ -1940,7 +1940,7 @@ status_t DashPlayer::setParameter(int key, const Parcel &request)
             return NO_MEMORY;
         }
 
-        utf16_to_utf8(str, len, (char*) data);
+        utf16_to_utf8(str, len, (char*) data, len);
         err = mSource->setParameter(key, data, len);
         free(data);
     }else if(key == KEY_DASH_QOE_EVENT)
